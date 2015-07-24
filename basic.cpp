0251cab0ba793dd81aaa4db651ae21dfae31bf41
Nicole Cronin <Nicki@adm-129-49-192-170.wi-fi.stonybrook.edu>
2015-07-24 15:11:41 -0400

    adding some input

--- basic.cpp
+++ basic.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
 int main(){
-  int a=1;
-  std::cout << "a= " << a << std::endl;
-  if(a==1)
+  int a=0;
+  std::cout << "write a number: [0-10]: ";
+  std::cin >> a;
+  if(a>10 || a<0)
     {
-      a=2;
+      std::cout << "you don't like following instructions, do you?" << std::endl;
     }
   else
     {
-      //this part never executes
+      std::cout << "thanks!" << std::endl;
     }
 
   std::cout << "a= " << a << std::endl;
